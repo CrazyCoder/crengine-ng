05d804b5ce66559dfa70d6fe9966c1cdc71ed02e
Aleksey Chernov <valexlin@gmail.com>
2022-12-14 01:31:56 +0400

    Update source file headers in crengine/include/: use the standard GPLv2+ header. File authors reconstructed.

--- crengine/include/ldomnode.h
+++ crengine/include/ldomnode.h
@@ -1,13 +1,25 @@
-/**
- * CoolReader Engine
- *
- * (c) Vadim Lopatin, 2000-2011
- * (c) Other CoolReader authors (See AUTHORS file)
- *
- * This source code is distributed under the terms of
- * GNU General Public License version 2
- * See LICENSE file for details
- */
+/***************************************************************************
+ *   crengine-ng                                                           *
+ *   Copyright (C) 2007-2012 Vadim Lopatin <coolreader.org@gmail.com>      *
+ *   Copyright (C) 2019,2020 Konstantin Potapov <pkbo@users.sourceforge.net>
+ *   Copyright (C) 2018,2020 Aleksey Chernov <valexlin@gmail.com>          *
+ *   Copyright (C) 2018-2021 poire-z <poire-z@users.noreply.github.com>    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License           *
+ *   as published by the Free Software Foundation; either version 2        *
+ *   of the License, or (at your option) any later version.                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA.                                                   *
+ ***************************************************************************/
 
 #ifndef __LDOMNODE_H_INCLUDED__
 #define __LDOMNODE_H_INCLUDED__
