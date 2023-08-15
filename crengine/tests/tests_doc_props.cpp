b8bfc586bc4da2600a78e1cd6c1220bd8a8fcda2
Aleksey Chernov <valexlin@gmail.com>
2023-08-15 18:56:05 +0400

    Simplify DocPropsTests

--- crengine/tests/tests_doc_props.cpp
+++ crengine/tests/tests_doc_props.cpp
@@ -32,159 +32,155 @@
 #error Please define TESTS_DATADIR, which points to the directory with the data files for the tests
 #endif
 
-#ifndef RENDER_REFERENCE_DIR
-#error Please define RENDER_REFERENCE_DIR, which points to the directory with the render reference files
-#endif
-
-// Fixtures
-
-class DocPropsTests: public testing::Test
-{
-protected:
-    LVDocView* m_view;
-    CRPropRef m_props;
-protected:
-    DocPropsTests()
-            : testing::Test() {
-        m_view = NULL;
-    }
-    virtual void SetUp() override {
-        m_view = new LVDocView(32, false);
-    }
-
-    virtual void TearDown() override {
-        if (m_view) {
-            delete m_view;
-            m_view = 0;
-        }
-    }
-};
-
 // units tests
 
-TEST_F(DocPropsTests, GetDocAuthorsOneInFB2) {
+TEST(DocPropsTests, GetDocAuthorsOneInFB2) {
     CRLog::info("==============================");
     CRLog::info("Starting GetDocAuthorsOneInFB2");
 
     // open document
-    ASSERT_TRUE(m_view->LoadDocument(TESTS_DATADIR "testprops-one-author.fb2"));
+    LVDocView* view = new LVDocView(32, false);
+    ASSERT_TRUE(view->LoadDocument(TESTS_DATADIR "testprops-one-author.fb2"));
 
     // Get author(s)
-    lString32 authors = m_view->getAuthors();
+    lString32 authors = view->getAuthors();
 
     EXPECT_STREQ(LCSTR(authors), "First Author");
+
+    delete view;
 
     CRLog::info("Finished GetDocAuthorsOneInFB2");
     CRLog::info("==============================");
 }
 
-TEST_F(DocPropsTests, GetDocAuthorsTwoInFB2) {
+TEST(DocPropsTests, GetDocAuthorsTwoInFB2) {
     CRLog::info("==============================");
     CRLog::info("Starting GetDocAuthorsTwoInFB2");
 
     // open document
-    ASSERT_TRUE(m_view->LoadDocument(TESTS_DATADIR "testprops-two-authors.fb2"));
+    LVDocView* view = new LVDocView(32, false);
+    ASSERT_TRUE(view->LoadDocument(TESTS_DATADIR "testprops-two-authors.fb2"));
 
     // Get author(s)
-    lString32 authors = m_view->getAuthors();
+    lString32 authors = view->getAuthors();
 
     EXPECT_STREQ(LCSTR(authors), "First Author, Second G. Author");
+
+    delete view;
 
     CRLog::info("Finished GetDocAuthorsTwoInFB2");
     CRLog::info("==============================");
 }
 
-TEST_F(DocPropsTests, GetDocGenresOneInFB2) {
+TEST(DocPropsTests, GetDocGenresOneInFB2) {
     CRLog::info("=============================");
     CRLog::info("Starting GetDocGenresOneInFB2");
 
     // open document
-    ASSERT_TRUE(m_view->LoadDocument(TESTS_DATADIR "testprops-one-genre.fb2"));
+    LVDocView* view = new LVDocView(32, false);
+    ASSERT_TRUE(view->LoadDocument(TESTS_DATADIR "testprops-one-genre.fb2"));
 
     // Get genres
-    lString32 genres = m_view->getKeywords();
+    lString32 genres = view->getKeywords();
 
     EXPECT_STREQ(LCSTR(genres), "comp_programming");
+
+    delete view;
 
     CRLog::info("Finished GetDocGenresOneInFB2");
     CRLog::info("=============================");
 }
 
-TEST_F(DocPropsTests, GetDocGenresTwoInFB2) {
+TEST(DocPropsTests, GetDocGenresTwoInFB2) {
     CRLog::info("=============================");
     CRLog::info("Starting GetDocGenresTwoInFB2");
 
     // open document
-    ASSERT_TRUE(m_view->LoadDocument(TESTS_DATADIR "testprops-two-genres.fb2"));
+    LVDocView* view = new LVDocView(32, false);
+    ASSERT_TRUE(view->LoadDocument(TESTS_DATADIR "testprops-two-genres.fb2"));
 
     // Get genres
-    lString32 genres = m_view->getKeywords();
+    lString32 genres = view->getKeywords();
 
     EXPECT_STREQ(LCSTR(genres), "comp_programming, comp_soft");
+
+    delete view;
 
     CRLog::info("Finished GetDocGenresTwoInFB2");
     CRLog::info("=============================");
 }
 
-TEST_F(DocPropsTests, GetDocAuthorsOneInEPUB) {
+TEST(DocPropsTests, GetDocAuthorsOneInEPUB) {
     CRLog::info("===============================");
     CRLog::info("Starting GetDocAuthorsOneInEPUB");
 
     // open document
-    ASSERT_TRUE(m_view->LoadDocument(TESTS_DATADIR "testprops-one-author.epub"));
+    LVDocView* view = new LVDocView(32, false);
+    ASSERT_TRUE(view->LoadDocument(TESTS_DATADIR "testprops-one-author.epub"));
 
     // Get author(s)
-    lString32 authors = m_view->getAuthors();
+    lString32 authors = view->getAuthors();
 
     EXPECT_STREQ(LCSTR(authors), "First Author");
+
+    delete view;
 
     CRLog::info("Finished GetDocAuthorsOneInEPUB");
     CRLog::info("===============================");
 }
 
-TEST_F(DocPropsTests, GetDocAuthorsTwoInEPUB) {
+TEST(DocPropsTests, GetDocAuthorsTwoInEPUB) {
     CRLog::info("===============================");
     CRLog::info("Starting GetDocAuthorsTwoInEPUB");
 
     // open document
-    ASSERT_TRUE(m_view->LoadDocument(TESTS_DATADIR "testprops-two-authors.epub"));
+    LVDocView* view = new LVDocView(32, false);
+    ASSERT_TRUE(view->LoadDocument(TESTS_DATADIR "testprops-two-authors.epub"));
 
     // Get author(s)
-    lString32 authors = m_view->getAuthors();
+    lString32 authors = view->getAuthors();
 
     EXPECT_STREQ(LCSTR(authors), "First Author, Second Good Author");
+
+    delete view;
 
     CRLog::info("Finished GetDocAuthorsTwoInEPUB");
     CRLog::info("===============================");
 }
 
-TEST_F(DocPropsTests, GetDocKeywordsOneInEPUB) {
+TEST(DocPropsTests, GetDocKeywordsOneInEPUB) {
     CRLog::info("================================");
     CRLog::info("Starting GetDocKeywordsOneInEPUB");
 
     // open document
-    ASSERT_TRUE(m_view->LoadDocument(TESTS_DATADIR "testprops-one-keyword.epub"));
+    LVDocView* view = new LVDocView(32, false);
+    ASSERT_TRUE(view->LoadDocument(TESTS_DATADIR "testprops-one-keyword.epub"));
 
     // Get keywords
-    lString32 keywords = m_view->getKeywords();
+    lString32 keywords = view->getKeywords();
 
     EXPECT_STREQ(LCSTR(keywords), "programming");
+
+    delete view;
 
     CRLog::info("Finished GetDocKeywordsOneInEPUB");
     CRLog::info("================================");
 }
 
-TEST_F(DocPropsTests, GetDocKeywordsTwoInEPUB) {
+TEST(DocPropsTests, GetDocKeywordsTwoInEPUB) {
     CRLog::info("================================");
     CRLog::info("Starting GetDocKeywordsTwoInEPUB");
 
     // open document
-    ASSERT_TRUE(m_view->LoadDocument(TESTS_DATADIR "testprops-two-keywords.epub"));
+    LVDocView* view = new LVDocView(32, false);
+    ASSERT_TRUE(view->LoadDocument(TESTS_DATADIR "testprops-two-keywords.epub"));
 
     // Get keywords
-    lString32 keywords = m_view->getKeywords();
+    lString32 keywords = view->getKeywords();
 
     EXPECT_STREQ(LCSTR(keywords), "programming, soft");
+
+    delete view;
 
     CRLog::info("Finished GetDocKeywordsTwoInEPUB");
     CRLog::info("================================");
